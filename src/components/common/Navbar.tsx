79a6742567dc1b2239c65ca33179c81b6a5529d2
lucian <lucianxcv@gmail.com>
2025-08-24 14:53:09 -0700

    small logo change

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -306,7 +306,7 @@ export const Navbar: React.FC<NavbarProps> = ({
                 transition: 'all 0.3s ease'
               }}
             />
-            <span>Wednesday Yachting Luncheon</span>
+            <span>WYL</span>
           </Link>
 
           {/* Desktop Navigation */}
